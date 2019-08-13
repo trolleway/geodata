d785341c45462d0430c3a3f942283ec2781358b3
Artem Svetlov <trolleway@yandex.ru>
2019-08-13 23:23:39 +0200

    Update data_prepare.py

--- wikipedia-kyoto/data_prepare.py
+++ wikipedia-kyoto/data_prepare.py
@@ -15,16 +15,60 @@ if not os.path.isfile(filename):
     with open(filename, 'wb') as f:
         f.write(r.content)
 else:
-    print(filename+' alreany exist, no downloading')
+    print(filename+' already exist, no downloading')
 
 #clip
 #'''osmconvert.exe c:\Users\trolleway\Downloads\kansai-latest.osm.pbf -b=135.571993,34.926126,135.960634,35.144269 --complete-multipolygons  --complete-ways --drop-author -o=c:\Users\trolleway\Downloads\kyoto.osm'''
 
 print('clip')
-clipped = 'clipped.osm.pbf'
+clipped = 'clipped.o5m'
 
 cmd = 'osmconvert {source} -b={bbox} --complete-multipolygons  --complete-ways --drop-author -o={o}'
 cmd = cmd.format(source=filename,bbox=bbox,o=clipped)
 print(cmd)
 os.system(cmd)
                  
+#filters
+
+'''
+points
+lines
+multilinestrings
+multipolygons
+other_relations
+'''
+filters=list()
+filter={}
+filter['filter'] = 'highway=trunk'
+filter['name'] = 'road freeway'
+filter['geom_type'] = 'lines'
+filters.append(filter)
+
+filter={}
+filter['filter'] = 'highway=motorway'
+filter['name'] = 'road major'
+filter['geom_type'] = 'lines'
+filters.append(filter)
+
+filter={}
+filter['filter'] = 'highway=primary'
+filter['name'] = 'road mainor'
+filter['geom_type'] = 'lines'
+filters.append(filter)
+
+filter={}
+filter['filter'] = 'railway=rail
+filter['name'] = 'rail'
+filter['geom_type'] = 'lines'
+filters.append(filter)
+
+
+#filtering
+for filter in filters:
+    cmd = 'osmfilter {source} --keep-tags="{filter}" -o={output}.osm'
+    cmd = cmd.format(source=clipped,filter=filter['filter'],output=filter['name'])
+    print(cmd)
+    os.system(cmd)
+    
+    cmd='ogr2ogr -f GeoJSON -overwrite {name}.geojson {name}.osm {geom_type}'
+    cmd = cmd.format(name=filter['name'],geom_type=filter['geom_type'])
